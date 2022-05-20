9b52143141f69df4f540d6de2ad4f540b906cf49
Ritter Gustave <ritter.gustave@sureprep.com>
2022-05-19 21:10:32 -0700

    build(initial project base): base which includes tests setup, commit setup, etc

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+module.exports = {
+	roots: ["<rootDir>/src"],
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	transform: {
+		"\\.[jt]sx?$": "babel-jest",
+	},
+	globals: {
+		'ts-jest': {
+			babelConfig: require('./babel.config'),
+		},
+	},
+	moduleNameMapper: {
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+		"\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+	},
+};
---
+++ src/test/app.test.tsx
@@ -0,0 +1,8 @@
+import App from '../App';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+test('Renders the App I guess....', () => {
+	const { getByText } = render(<App />);
+	expect(getByText('Vite Docs')).toBeInTheDocument();
+});

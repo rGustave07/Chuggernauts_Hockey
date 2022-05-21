5c3286a59c0b84112018f57d42dd0009a6805668
Jake Hamilton <jake.hamilton@hey.com>
2022-05-21 16:57:15 -0700

    chore(tests): use vitest for unit testing

--- src/components/Blank/__tests__/Blank.test.tsx
+++ src/components/Blank/__tests__/Blank.test.tsx
@@ -1,8 +1,7 @@
 import Blank from "../Blank";
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
 
 test("Literally renders nothing, lol", () => {
 	const { queryByText } = render(<Blank />);
-	expect(queryByText("anything")).not.toBeInTheDocument();
+	expect(queryByText("anything")).toBe(null);
 });

b1f84e1f184689e65f9f14999e1c499dacad37d5
rGustave07 <rGustave5@gmail.com>
2022-05-23 17:54:39 -0700

    test(test modules): add configuration for jest to correctly find modules

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,23 @@
 /* eslint-disable max-len */
 module.exports = {
-	roots: ['<rootDir>/src'],
-	preset: 'ts-jest',
+	roots: ["<rootDir>/src"],
+	preset: "ts-jest",
 	verbose: true,
-	testEnvironment: 'jsdom',
+	testEnvironment: "jsdom",
 	transform: {
-		'\\.[jt]sx?$': 'babel-jest',
+		"\\.[jt]sx?$": "babel-jest",
 	},
-	testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-	testMatch: ['<rootDir>/src/**/__tests__/*.ts?(x)'],
+	testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+	testMatch: ["<rootDir>/src/**/__tests__/*.ts?(x)"],
 	globals: {
-		'ts-jest': {
-			babelConfig: require('./babel.config'),
+		"ts-jest": {
+			babelConfig: require("./babel.config"),
 		},
 	},
+	moduleDirectories: ["node_modules", "src"],
 	moduleNameMapper: {
-		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-		'\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+			"<rootDir>/__mocks__/fileMock.js",
+		"\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
 	},
 };

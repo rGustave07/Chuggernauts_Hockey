7c0505c2d4c2c2f107717f8d7f750e4503374889
rGustave07 <rGustave5@gmail.com>
2022-06-29 19:39:27 -0700

    refactor(lines page): add start for lines page

--- src/pages/Lines/Lines.tsx
+++ src/pages/Lines/Lines.tsx
@@ -1,10 +1,15 @@
 import TopNav from "components/TopNav";
+import PaddedContainer from "components/Containers/PaddedContainer";
+import Accordion from "components/Accordion";
 
 const Lines = (): JSX.Element => {
 	return (
 		<>
 			<TopNav />
-			<span>Lines page placeholder</span>
+			<h1 className="text-center prose-2xl p-10 lg:p-20 font-bold">Lines</h1>
+			<PaddedContainer>
+				<Accordion />
+			</PaddedContainer>
 		</>
 	);
 };
